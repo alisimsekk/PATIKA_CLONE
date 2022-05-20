374b62df6405b95ba9437b3575c40550cd9408e1
alisimsek <ali.simsek035@gmail.com>
2022-05-20 17:24:37 +0300

    update 2

--- src/com/patikadev/View/OperatorGUI.java
+++ src/com/patikadev/View/OperatorGUI.java
@@ -283,6 +283,7 @@ public class OperatorGUI extends JFrame {
 
         btn_logout.addActionListener(e -> {
             dispose();
+            LoginGUI login = new LoginGUI();
         });
 
         btn_patika_add.addActionListener(e -> {
